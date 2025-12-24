17da2aa1fa3d504e6a60a71582c158065bb1b5a6
uanela <uanelaluiswayne@gmail.com>
2025-12-24 12:30:02 +0200

    chore: aula de retorno html e json

--- 2-modulo/final/server.js
+++ 2-modulo/final/server.js
@@ -2,11 +2,26 @@ import http from "http";
 
 // Criando o servidor http
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { "Content-type": "text/plain" });
-  res.end("Hello World!");
+  const obj = {
+    name: "Uanela",
+    age: "40",
+    country: "Moçambique",
+  };
+
+  // JSON - JavaScript Object Notation
+
+  res.writeHead(200, { "Content-type": "application/json" });
+  res.end(`{
+    "firstName": "Uanela",
+    "lastName": "Como",
+    "age": "45",
+    "country: "Moçambique"
+    }`);
 });
 
+const PORT = 3001;
+
 // Configurando para escutar requisições na porta 3000
-server.listen(3000, () => {
-  console.log(`Servidor rodando em http://localhost:3000`);
+server.listen(PORT, () => {
+  console.log(`Servidor rodando em http://localhost:${PORT}`);
 });
