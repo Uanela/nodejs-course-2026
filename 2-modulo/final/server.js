da2f667e682a62c5e1ebf0c32e03d38bdf64e416
uanela <uanelaluiswayne@gmail.com>
2025-12-24 13:05:11 +0200

    aula-14: metodos https e rotas na pratica

--- 2-modulo/final/server.js
+++ 2-modulo/final/server.js
@@ -1,25 +1,39 @@
 import http from "http";
 
+// Metódos HTTP
+// GET - Buscar/Ler/Pegar
+// POST - Criar
+// PUT - Atualizar todos os campos
+// PATCH - Atualizar parcial
+// DELETE - Apagar recurso
+
+// Rotas
+// /produtos
+// /usuarios
+
+const database = {
+  produtos: ["Samsung S30 Ultra", "Macbook M20", "Playstation 10"],
+};
+
 // Criando o servidor http
 const server = http.createServer((req, res) => {
-  // Metódos HTTP
-  // GET - Buscar/Ler/Pegar
-  // POST - Criar
-  // PUT - Atualizar todos os campos
-  // PATCH - Atualizar parcial
-  // DELETE - Apagar recurso
+  const { method, url } = req;
 
-  // Rotas
-  // /produtos
-  // /usuarios
-
+  // Definindo o tipo de resposta que será enviado
   res.writeHead(200, { "Content-type": "application/json" });
-  res.end(`{
-    "firstName": "Uanela",
-    "lastName": "Como",
-    "age": "45",
-    "country: "Moçambique"
-    }`);
+
+  // Fazendo log de dados da requisição
+  console.log(req.method, new Date(Date.now()).toLocaleTimeString(), req.url);
+
+  if (method === "GET") {
+    if (url === "/api/produtos") {
+      return res.end(`{ "produtos": ["Produto 1", "Produto 2"] }`);
+    } else if (url === "/api/carros") {
+      return res.end(`{ "carros": ["Toyota", "Tesla", "Ferrari"] }`);
+    }
+  }
+
+  res.end(`{ mensagem: "Rota não encontrada!" }`);
 });
 
 const PORT = 3001;
