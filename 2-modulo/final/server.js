0fea4cac4d9a14640d30c431444c1b40e77e9fee
uanela <uanelaluiswayne@gmail.com>
2025-12-24 14:13:11 +0200

    aula-15-e-16: CRUD em NodeJs e Refactoriazacao

--- 2-modulo/final/server.js
+++ 2-modulo/final/server.js
@@ -12,28 +12,64 @@ import http from "http";
 // /usuarios
 
 const database = {
-  produtos: ["Samsung S30 Ultra", "Macbook M20", "Playstation 10"],
+  produtos: [
+    { id: 1, nome: "Samsung S30 Ultra", preco: 180 },
+    { id: 2, nome: "Macbook M20", preco: 299 },
+    { id: 3, nome: "Playstation 10", preco: 100 },
+  ],
+  carros: [
+    { id: 1, nome: "Toyota Corolla", modelo: "XEi 2.0", preco: 120 },
+    { id: 2, nome: "Honda Civic", modelo: "Touring 1.5 Turbo", preco: 150 },
+    { id: 3, nome: "Volkswagen Jetta", modelo: "GLI 2.0 TSI", preco: 135 },
+  ],
 };
 
 // Criando o servidor http
 const server = http.createServer((req, res) => {
   const { method, url } = req;
 
-  // Definindo o tipo de resposta que será enviado
-  res.writeHead(200, { "Content-type": "application/json" });
+  // Headers
+  let statusCode = 200;
+  let contentType = "application/json";
 
-  // Fazendo log de dados da requisição
-  console.log(req.method, new Date(Date.now()).toLocaleTimeString(), req.url);
+  // Response body
+  let responseBody = { mensagem: "Rota não encontrada" };
 
   if (method === "GET") {
+    // GET /api/produtos - Retona a lista de produtos
     if (url === "/api/produtos") {
-      return res.end(`{ "produtos": ["Produto 1", "Produto 2"] }`);
+      responseBody = { produtos: database.produtos };
+      // GET /api/carros - Retona a lista de carros
     } else if (url === "/api/carros") {
-      return res.end(`{ "carros": ["Toyota", "Tesla", "Ferrari"] }`);
+      responseBody = { carros: database.carros };
+
+      // GET /api/carros/:id - Buscar um carro atráves do ID
+    } else if (url.startsWith("/api/carros/")) {
+      const id = Number(url.split("/")[3]);
+      const carro = database.carros.find((c) => c.id === id);
+
+      if (carro) {
+        responseBody = { carro };
+      } else {
+        statusCode = 404;
+        responseBody = { mensagem: `Carro com id ${id} não encontrado.` };
+      }
     }
   }
 
-  res.end(`{ mensagem: "Rota não encontrada!" }`);
+  if (responseBody?.mensagem === "Rota não encontrada") statusCode = 404;
+
+  // Definindo o tipo de resposta (usando header) que será enviado
+  res.writeHead(statusCode, { "Content-type": contentType });
+  res.end(JSON.stringify(responseBody));
+
+  // Fazendo log de dados da requisição
+  console.log(
+    req.method,
+    new Date(Date.now()).toLocaleTimeString(),
+    req.url,
+    statusCode
+  );
 });
 
 const PORT = 3001;
